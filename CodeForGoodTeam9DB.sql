30728ebca872cbdb6c865a07a6d0309ccb2bfe6f
Rory Kelly <rorykelly47@gmail.com>
2019-11-02 00:00:17 +0000

    Update CodeForGoodTeam9DB.sql

--- CodeForGoodTeam9DB.sql
+++ CodeForGoodTeam9DB.sql
@@ -50,8 +50,8 @@ CREATE TABLE IF NOT EXISTS `codeforgoodteam9`.`universities` (
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
 COMMENT = '		';
 
 USE `codeforgoodteam9` ;
