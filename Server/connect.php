3a542c1773096dfb7066f213af446e044237fab5
JordanAherne1 <47026136+JordanAherne1@users.noreply.github.com>
2019-11-02 06:06:34 +0000

    Update connect.php

--- Server/connect.php
+++ Server/connect.php
@@ -4,7 +4,7 @@ function OpenCon()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "1234";
- $db = "example";
+ $db = "codeforgoodteam9";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
